ee49b0bdde73e4feae2570be7f5c433be2f8b971
Stevany Angel <stevanyangel_2327240033@mhs.mdp.ac.id>
2025-11-15 18:38:54 +0700

    axios

--- Movie-React/src/pages/movies/AddMovie.tsx
+++ Movie-React/src/pages/movies/AddMovie.tsx
@@ -7,7 +7,7 @@ function AddMovie() {
         <h2>Add Movie Page</h2>
         <NavLink to="/">Movie Page</NavLink>
         </div>
-        <Button variant="btn btn-primary"> Add </Button>
+        <Button variant="btn btn-primary"> List Movie </Button>
     </div>
     
 }
