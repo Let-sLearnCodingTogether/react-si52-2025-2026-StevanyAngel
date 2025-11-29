6d6a0ab79c00e1a686f97f18db78b370d4b17992
Stevany Angel <stevanyangel_2327240033@mhs.mdp.ac.id>
2025-11-29 17:19:16 +0700

    update movie-express

--- movie-express/controllers/movieController.js
+++ movie-express/controllers/movieController.js
@@ -1,16 +1,59 @@
+import mongoose from "mongoose";
 import movieModel from "../models/movieModel.js";
 
 export const listMovie = async (req, res) => {
   try {
-    const data = await movieModel.find({});
+    // Hanya menampilkan movie milik user yang sedang login
+    const movie = await movieModel
+      .find({
+        createdBy: req.user?.user_id,
+      })
+      .sort({ createdAt: -1 });
 
+    return res.status(200).json({
+      message: "Daftar semua movie",
+      data: movie,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Terjadi kesalahan pada server",
+      error: error.message,
+      data: null,
+    });
+  }
+};
+
+export const getMovieById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+      // Tambahan validasi ID Mongoose
+      return res.status(400).json({
+        message: "ID Movie wajib diisi dan harus valid",
+        data: null,
+      });
+    }
+
+    // Mencari movie berdasarkan ID dan Kepemilikan user
+    const movie = await movieModel.findOne({
+      _id: id,
+      createdBy: req.user?.user_id,
+    });
+
+    if (!movie) {
+      return res.status(404).json({
+        message: "Movie tidak ditemukan",
+        data: null,
+      });
+    }
     res.status(200).json({
-      message: "Berhasil mendapatkan list movie",
-      data: data,
+      message: "Berhasil mendapatkan data Movie",
+      data: movie,
     });
   } catch (error) {
     res.status(500).json({
-      message: error.message,
+      message: "Terjadi kesalahan pada server",
+      error: error.message,
       data: null,
     });
   }
@@ -18,7 +61,17 @@ export const listMovie = async (req, res) => {
 
 export const addListMovie = async (req, res) => {
   try {
-    const newMovie = await movieModel.create(req.body);
+    const { judul, tahunRilis, sutradara } = req.body;
+
+    if (!judul || !tahunRilis || !sutradara) {
+      return res.status(400).json({
+        message: "Semua field (judul, tahunRilis, sutradara) wajib diisi",
+        data: null,
+      });
+    }
+
+    // Menyimpan user_id pembuat ke database
+    const newMovie = await movieModel.create({ judul, tahunRilis, sutradara, createdBy: req.user?.user_id });
 
     res.status(201).json({
       message: "Movie berhasil ditambahkan",
@@ -34,28 +87,37 @@ export const addListMovie = async (req, res) => {
 
 export const updateDataMovie = async (req, res) => {
   try {
-    const id = req.params?.id;
-    const request = req.body;
+    const { id } = req.params;
+    const { judul, tahunRilis, sutradara } = req.body;
 
-    if (!id) {
+    if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+      // FIX: Validasi ID
       return res.status(400).json({
-        message: "ID Movie wajib diisi di parameter URL",
+        message: "ID Movie wajib diisi di parameter URL dan harus valid",
         data: null,
       });
     }
 
-    const response = await movieModel.findByIdAndUpdate(id, request, { new: true });
+    // FIX: Menggunakan findOneAndUpdate
+    const updateMovie = await movieModel.findOneAndUpdate(
+      {
+        _id: id,
+        createdBy: req.user?.user_id, // FIX: Perbaiki typo createBy menjadi createdBy
+      },
+      { judul, tahunRilis, sutradara },
+      { new: true }
+    );
 
-    if (!response) {
+    if (!updateMovie) {
       return res.status(404).json({
-        message: "Data movie tidak ditemukan",
+        message: "Data movie tidak ditemukan atau Anda tidak memiliki izin untuk mengupdate",
         data: null,
       });
     }
 
     return res.status(200).json({
       message: "Data movie berhasil diupdate",
-      data: response,
+      data: updateMovie,
     });
   } catch (error) {
     res.status(500).json({
@@ -67,31 +129,37 @@ export const updateDataMovie = async (req, res) => {
 
 export const deleteMovie = async (req, res) => {
   try {
-    const id = req.params.id;
+    const { id } = req.params;
 
-    if (!id) {
+    if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+      // FIX: Validasi ID
       return res.status(400).json({
-        message: "ID Movie wajib diisi di parameter URL",
+        message: "ID Movie wajib diisi di parameter URL dan harus valid",
         data: null,
       });
     }
 
-    const response = await movieModel.findByIdAndDelete(id);
+    // FIX: Menggunakan findOneAndDelete
+    const deleteMovie = await movieModel.findOneAndDelete({
+      _id: id,
+      createdBy: req.user?.user_id,
+    });
 
-    if (!response) {
+    if (!deleteMovie) {
       return res.status(404).json({
-        message: "Movie tidak ditemukan",
+        message: "Movie tidak ditemukan atau Anda tidak memiliki izin untuk menghapus",
         data: null,
       });
     }
 
     return res.status(200).json({
       message: "Movie berhasil dihapus",
-      data: response,
+      data: deleteMovie,
     });
   } catch (error) {
     res.status(500).json({
-      message: error.message,
+      message: "Terjadi kesalahan pada server",
+      error: error.message,
       data: null,
     });
   }
--- movie-express/routes/api.js
+++ movie-express/routes/api.js
@@ -1,11 +1,23 @@
 import express from "express";
 import * as movieController from "../controllers/movieController.js";
+import * as userController from "../controllers/userController.js";
+import { authenticateTokenMiddleware } from "../middlewares/authenticateTokenMiddleware.js";
 
 const api = express.Router();
 
-api.get("/movie", movieController.listMovie);
-api.post("/movie", movieController.addListMovie);
-api.put("/movie/:id", movieController.updateDataMovie);
-api.delete("/movie/:id", movieController.deleteMovie);
+api.post("/signin", userController.signIn);
+api.post("/signup", userController.signUp);
+
+// Protected Routes (Movies)
+// api.get("/movie", movieController.listMovie);
+// api.post("/movie", movieController.addListMovie);
+// api.put("/movie/:id", movieController.updateDataMovie);
+// api.delete("/movie/:id", movieController.deleteMovie);
+
+api.get("/movie", authenticateTokenMiddleware, movieController.listMovie);
+api.get("/movie/:id", authenticateTokenMiddleware, movieController.getMovieById);
+api.post("/movie", authenticateTokenMiddleware, movieController.addListMovie);
+api.put("/movie/:id", authenticateTokenMiddleware, movieController.updateDataMovie);
+api.delete("/movie/:id", authenticateTokenMiddleware, movieController.deleteMovie);
 
 export default api;
